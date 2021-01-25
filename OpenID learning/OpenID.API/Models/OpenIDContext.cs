3cb7e9074de5df4bc9f71da8cbc0e63e3c1979c1
Patryk Paziewski <patryk@duetzdronem.pl>
2021-01-25 15:59:55 +0100

    Project created

---
+++ OpenID learning/OpenID.API/Models/OpenIDContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenID.API.Models
+{
+    public class OpenIDContext : DbContext
+    {
+        public OpenIDContext(DbContextOptions<OpenIDContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    } 
+}
